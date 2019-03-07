cf971e18bcb445eb14b2f2c8ccb4ca2f40bba15d
Nicolas Kamennoff <Nicolas Kamennoff>
2019-03-07 05:23:05 -0800

    initial commit

---
+++ lib/panda.ex
@@ -0,0 +1,22 @@
+defmodule Panda do
+  @moduledoc """
+  Panda is a test program for Pandascore interview process.
+  This module implements the following required methods:
+    - upcoming_matches
+    - odds_for_match
+  """
+
+  @doc """
+  return table with the first five upcoming matches on PandaScore API
+  """
+  def upcoming_matches do
+    :todo
+  end
+
+  @doc """
+  return odds for the given match
+  """
+  def odds_for_match(match) do
+    "odds for the match #{match}"
+  end
+end
